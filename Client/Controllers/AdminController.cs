c29b74f8c464054814322a6a40d5d43d7fe3b816
rizki_fauzi24 <60428499+rizkifauzi24@users.noreply.github.com>
2022-07-03 22:55:31 +0700

    #Update LMS V3

---
+++ Client/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    //[Authorize]
+    public class AdminController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult TopicData()
+        {
+            return View();
+        }
+        
+        public IActionResult CourseData()
+        {
+            return View();
+        }
+        public IActionResult SectionData()
+        {
+            return View();
+        }
+        public IActionResult TransactionData()
+        {
+            return View();
+        }
+        public IActionResult UserData()
+        {
+            return View();
+        }
+    }
+}
